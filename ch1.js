033d631b95e7e32f16bb6f7a125015d55e14d388
roma <roma@funatix.club>
2020-08-04 19:04:07 +0300

    Motivation slider:

--- ch1.js
+++ ch1.js
@@ -1,2 +1,4 @@
 console.log('start in good time');
 console.log('happy new day');
+
+
---
+++ list.js
@@ -0,0 +1,12 @@
+class List {
+
+    constructor() {
+        this.pos = 0;
+        this.length = 0;
+        this.listSize = 0;
+        this.data = [];
+    }
+}
+
+let tempList = new List();
+console.log(tempList);
